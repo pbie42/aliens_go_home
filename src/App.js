8dbee30ca69645409147f3446924537ff63dabbd
Paul BIE <pbie@student.42.fr>
2018-03-30 19:16:24 +0200

    [Add] - Redux to app with store. Game container for app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react"
+import PropTypes from "prop-types"
 import "./App.css"
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<header className="App-header">
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+				<h1>{this.props.message}</h1>
 			</div>
 		)
 	}
+}
+
+App.propTypes = {
+	message: PropTypes.string.isRequired
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import './index.css'
+import Game from './containers/Game'
+import reducer from './reducers'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(
+	reducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+ReactDOM.render(
+	<Provider store={store}>
+		<Game />
+	</Provider>,
+	document.getElementById('root')
+)
+registerServiceWorker()
